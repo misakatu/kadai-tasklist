b03efd0126b6c4be91dd1bcd22f836730cbaccf6
miso.katu <kanashikuang@gmail.com>
2020-09-13 15:39:02 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   get 'toppages/index'
-  root to: 'tasks#index'
+#  root to: 'tasks#index'
   
   root to: 'toppages#index'
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:index, :show, :new, :create]
 
   resources :tasks
 end
