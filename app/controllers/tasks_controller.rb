676cb2dd353ac8ac9b69fabe3c5530414d4d2b82
miso.katu <kanashikuang@gmail.com>
2020-09-18 13:34:37 +0000

    fix page4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_tasks, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:show, :new, :edit]
+  before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
 
   
   def index
